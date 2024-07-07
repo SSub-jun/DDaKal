e5df205d2b7e3ae0023d5df72ab5e0c0e549d166
SANGDOLEE <xkdxh625@naver.com>
2024-07-07 17:25:13 +0900

    Feat: EditView NavigationTitle("수정하기")

--- DancingMarker/WatchDancingMarker Watch App/View/WatchMarkerEditView.swift
+++ DancingMarker/WatchDancingMarker Watch App/View/WatchMarkerEditView.swift
@@ -2,8 +2,21 @@
 import SwiftUI
 
 struct WatchMarkerEditView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                
+                
+            }
+            .navigationTitle {
+                Text("수정하기")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.yellow)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
     }
 }
 
