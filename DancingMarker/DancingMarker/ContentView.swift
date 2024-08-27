59fd4e9efd6daaf29c88bb73ad293637bc5228e0
won <mosiccan@gmail.com>
2024-08-27 16:01:03 +0900

    feat: #39 백그라운드 제거, 배속 버튼 ui 수정
    - 배속이 양쪽 끝 (0.5, 1.5) 일때 disabled처리 합니다.

--- DancingMarker/DancingMarker/ContentView.swift
+++ DancingMarker/DancingMarker/ContentView.swift
@@ -14,35 +14,21 @@ struct ContentView: View {
     @State private var currentView: PathType? = nil
     @Query var musicList: [Music] = []
     @EnvironmentObject var playerModel: PlayerModel
-
+    
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
             VStack {
-                //MARK: - 스플래시 화면 구현 필요
-                switch currentView {
-                case .musicList:
-                    MusicListView()
-                default: Text("Drop The Beat(로고)")
-                        .font(.largeTitle)
-                        .padding()
-                }
+                MusicListView()
+                
             }
             .navigationDestination(for: PathType.self) { pathType in
                 pathType.NavigatingView()
             }
-
+            
         }
         .environment(navigationManager)
         .onAppear {
-            resetShowMusicList()
             playerModel.sendMusicListToWatch(with: musicList)
-        }
-    }
-    
-    private func resetShowMusicList() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            showMusicList = true
-            currentView = .musicList
         }
     }
 }
